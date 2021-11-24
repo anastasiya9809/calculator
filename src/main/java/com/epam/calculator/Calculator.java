46ea9e096a31f70dc3ab29a647e8677a3c0daedb
aausi <aausi@DESKTOP-2JVPSSP>
2021-11-24 03:08:54 +0300

    add methods to Calculator class and CalculatorTest class

--- src/main/java/com/epam/calculator/Calculator.java
+++ src/main/java/com/epam/calculator/Calculator.java
@@ -1,4 +1,19 @@
 package com.epam.calculator;
 
 public class Calculator {
+    public static int add(int first, int second){
+        return first + second;
+    }
+
+    public static int subtract(int first, int second){
+        return first - second;
+    }
+
+    public static int multiply(int first, int second){
+        return first * second;
+    }
+
+    public static int divide(int first, int second){
+        return first / second;
+    }
 }
